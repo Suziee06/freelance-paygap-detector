9e7e831808e67a5ddc778feb88143f4bd572b633
poorva-2619 <poorvaditya47@gmail.com>
2026-04-15 00:32:24 +0530

    Create main Spring Boot application entry point

---
+++ src/main/java/com/detector/FreelanceDetectorApplication.java
@@ -0,0 +1,12 @@
+package com.detector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreelanceDetectorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FreelanceDetectorApplication.class, args);
+    }
+}
